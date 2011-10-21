1f93beb8fe1ba4ac20b91c97f144814a298b2333
Anthony Lomeli <anthonylomeli11@gmail.com>
2011-10-20 20:16:03 -0700

    uploaded

---
+++ 1/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NotepadSharp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://anthony-lomeli.co.cc/notepad");
+        }
+    }
+}
